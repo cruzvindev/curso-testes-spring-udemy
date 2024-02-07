152247fbd47ca824b7e4fdc3423352ea34130b35
Vinicius Cruz <cruzvindev@gmail.com>
2024-02-07 18:39:10 -0300

    Terminando de fazer os testes de unidade

--- sw-planet-api/src/main/java/com/example/swplanetapi/domain/PlanetRepository.java
+++ sw-planet-api/src/main/java/com/example/swplanetapi/domain/PlanetRepository.java
@@ -1,10 +1,19 @@
 package com.example.swplanetapi.domain;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanetRepository extends JpaRepository<Planet, Long>{
+public interface PlanetRepository extends CrudRepository<Planet, Long>, QueryByExampleExecutor<Planet>{
     
+    Optional<Planet> findByName(String name);
 
+    @Override
+    <S extends Planet> List<S> findAll(Example<S> example);
 }
--- sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -2,16 +2,26 @@ package com.example.swplanetapi.domain;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
 
 @ExtendWith(MockitoExtension.class)
 //@SpringBootTest(classes = PlanetService.class)
@@ -33,5 +43,90 @@ public class PlanetServiceTest {
         Planet sut =  planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){                
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+        
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void getPlanet_ByExistingName_ReturnsPlanet(){
+        when(planetRepository.findByName("name")).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.getByName("name");
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingName_Empty(){
+        when(planetRepository.findByName("Pandora")).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.getByName("Pandora");
+
+        assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets(){
+        List<Planet> planets = new ArrayList<>() {{
+            add(PLANET);
+        }};
+
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+        when(planetRepository.findAll(query)).thenReturn(planets);
+
+        List<Planet> sut = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(1);
+        assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void listPlanets_ReturnsNoPlanets(){
+       //O método any faz com que o argumento do método seja irrelevante 
+        when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+
+        List<Planet> sut = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
+
+        assertThat(sut).isEmpty();
+    }
+
+    @Test 
+    public void removePlanet_WithExistingId_doesNotThrownAnyException(){
+        assertThatCode(() -> planetService.remove(1L)).doesNotThrowAnyException();
+    }
+
+    @Test 
+    public void removePlanet_WithUnexistingId_ThrowsException(){
+        //doThrow foi usado pois when não aceita um método void como parâmetro
+        doThrow(new RuntimeException()).when(planetRepository).deleteById(99L);
+
+        assertThatThrownBy(() -> planetService.remove(99L)).isInstanceOf(RuntimeException.class);
     }
 }
